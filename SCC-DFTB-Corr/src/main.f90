712f99ac4cd13a865aa7bbc32fde1f8b5928e033
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2012-03-08 17:50:19 +0000

    Makefile working with ifort
    Some function have been moved from main.f90 in utils.f90
    Added conversion factors in parameter.f90
    
    Added damping function in main.f90
    
    Now seem all working... maybe

--- SCC-DFTB-Corr/src/main.f90
+++ SCC-DFTB-Corr/src/main.f90
@@ -1,11 +1,12 @@
 PROGRAM SCC_Disp
   
   USE precision
+  USE utils
   USE read_tag_dftbp
   USE read_xyz
   USE read_atomdata
 !  USE parser_input  ! Not implemented - not indispensable
-!  USE parameter  ! Not implemented - not indispensable
+  USE parameter, only : BohrAngst,HartKcalMol
   IMPLICIT NONE
   integer(ki) :: natom
 
@@ -21,17 +22,29 @@ PROGRAM SCC_Disp
 
   real(kr),dimension(2,2) :: E
   real(kr),dimension(2)  :: C6ab
-
-  real(kr) :: Rab
+  
+  real(kr) :: Rab,ba,bb,bab,damp,b0,kkk
 
   integer(ki) :: i,j,k
 
   character(30) :: inputtagfile,inputcoofile,atomdatafile
   
+  b0 = 1
   inputtagfile = 'results.tag'
   inputcoofile = '91.xyz'
   atomdatafile = 'atomdata.data'
   
+  ! Mesure Units:
+  ! Ni -> Electron fraction
+  ! Coordinates -> Angstrom
+  ! Polarizability -> Angstrom^3
+  ! C6 coefficients -> Atomic Units
+  !
+  ! So I think the most simple thing is 
+  ! transform all dimension in atomic
+  ! units and, maybe, convert energy from 
+  ! Hartree/molecule in Kcal/mol
+
   ! Compute weights
   !
   !     W_i = N_i / Z_i
@@ -49,11 +62,43 @@ PROGRAM SCC_Disp
   !
   ! Compute Energy
   ! 
-  !    E_disp = sum_A,B (C6_AB / R_AB)
+  !    E_disp = sum_A sum_B>A F_damp (C6_AB / R_AB)
+  !
+  ! Which F_damp??
+  ! In TS's paper, is used a Fermi-type damping function:
+  !
+  !  f_damp = (1 + exp[ -d(R_ab/s*R0_ab -1)])^-1
+  !
+  ! They do a strange discussion about the way to choose R0_ab and R_ab
+  ! saying that the VdW by Bondi are radii for AIM and so we should not
+  ! use this as Radii for free atoms. 
+  ! Speaking with Stephan, "I" decided that it could be simpler start with 
+  ! simpler damping function: I will use as first Fdamp the one present in 
+  ! J Chem Teory Comp 7 3567 and called f_2n little modified: b(x) is defined as
+  ! F(x)*b_ij_asym while I assume F(x) = 1. At the end, I have:
+  ! 
+  ! f_dump_2n = 1 - exp(-x) * sum_k=0^2n x^k / k!
+  ! 
+  ! where
+  !
+  ! x = b_ij,asym * R_ij
+  !
+  ! where
+  ! 
+  ! b_ij,asym = 2 * b_ii,asym * b_jj,asym /( b_ii,asym + b_jj,asym )
+  !
+  ! where
+  ! 
+  ! b_ii,asym = b_0 * ( 1 / alpha_i,free)^(1/3) * ( V_i,free / V_i,AIM)^(1/3)
+  !
+  ! the last term, in this case, will be substituited by the "weight" calculated
+  ! in this routine:
+  !
+  ! V_i,free / V_i,AIM = Z_i / N_i (see above)
 
   ! If I would reduce memory consumption, I can compute the C6AIM 
   ! on-the-fly while I compute energy... In this first version I don't 
-  ! want to worry about memory consumptio... also because I have only 
+  ! want to worry about memory consumption... also because I have only 
   ! very little molecules.
 
   
@@ -69,16 +114,8 @@ PROGRAM SCC_Disp
   call get_atomdata(atomdatafile) ! Now I can use the atomdata array
   
   ! Retrive name of atom
-  !++ To understand what are the two molecules, I separe the red coords in two 
-  !++ set. Since I should have the same molecule twice into the file xyz, I assume
-  !++ that the first half of coords are associated at one molecule and the second
-  !++ are associated at the second.
   call get_coords(inputcoofile,natom)
-  if( mod(natom,2) /= 0 ) stop 'ERROR: check 1' ! controls
-  mol1 => coords(1:natom/2)      ! WARNING:: If you recall 'get_coords' these values change
-  mol2 => coords(natom/2:natom)  !
   
-
   if( Ni_size /= natom ) stop 'ERROR: check 1' ! Controls
   
   ! Compute C6AIM
@@ -88,7 +125,7 @@ PROGRAM SCC_Disp
      Zaim(i) = atomdata(findatom(coords(i)%atom_type))%Z          ! - Z_i of atoms in xyz file
      C6free(i,1) = atomdata(findatom(coords(i)%atom_type))%TSC6   ! - C6free by TS for "   "
      C6free(i,2) = atomdata(findatom(coords(i)%atom_type))%D3C6   ! - C6free by 3D for "   "
-     polar(i) = atomdata(findatom(coords(i)%atom_type))%polarizability ! -polar for    "   "
+     polar(i) = atomdata(findatom(coords(i)%atom_type))%polarizability / BohrAngst**3 ! -polar for    "   "
   end do
  
 !  write(*,'(I5 /)') (Zaim(i), i = 1,natom) ! debug
@@ -98,7 +135,7 @@ PROGRAM SCC_Disp
   
   ! => C6AIM
   do i = 1,2                                                     ! Compute the C6aim form
-     C6aim(:,i) = Ni(:) / Zaim(:) * C6free(:,i)                  ! both: TS and 3D
+     C6aim(:,i) = (Ni(:) / dble(Zaim(:)))**2 * C6free(:,i)                  ! both: TS and 3D
   end do                                                         ! 1 = TS ; 2 = 3D
 
 !  write(*,'(f8.3 /)') (C6eff(i,1), i = 1,natom) ! debug
@@ -109,22 +146,28 @@ PROGRAM SCC_Disp
 
   !!!!!!!!!!!!!!! COMPUTE C6BIAT & ENERGY !!!!!!!!!!!!!!!!
 
-  E = 0.d0
-  mol1loop : do i = 1,natom/2          ! loop on first molecule atoms
-     mol2loop : do j = natom/2,natom   ! loop on first molecule atoms
-        Rab = dist(coords(i)%coord,coords(j)%coord)
+  E = 0.0
+  atom1: do i = 1,natom
+     atom2: do j = i+1,natom
+        Rab = dist(coords(i)%coord,coords(j)%coord)/BohrAngst
+        ba = basym(b0,polar(i),Ni(i),Zaim(i))
+        bb = basym(b0,polar(j),Ni(j),Zaim(j))
+        bab = bmix(ba,bb)
+        damp = fdamp(bab,Rab)
+!        print*, i,j,Rab,bb,ba,damp ! debug
 !        print*, Rab, i ,j,coords(i)%coord,coords(j)%coord ! debug
-!        stop
+!        stop ! debug 
         TSvsD3loop : do k = 1,2  ! 1 => TS ; 2 => D3
            C6ab(1) = 2 * C6aim(i,k) * C6aim(j,k) / &
                 ( C6aim(i,k) + C6aim(j,k) )                  ! REL A
            C6ab(2) = 2 * C6aim(i,k) * C6aim(j,k) / &
                 ( ( polar(j) / polar(i) ) * C6aim(i,k) + &
-                  ( polar(i) / polar(j) ) * C6aim(j,k) )       ! REL B
-           E(:,k) = E(:,k) + C6ab(:) / Rab**6
+                ( polar(i) / polar(j) ) * C6aim(j,k) )       ! REL B
+           E(:,k) =  E(:,k) - damp * C6ab(:) / Rab**6                ! Not implemented damping function yet. 
         end do TSvsD3loop
-     end do mol2loop
-  end do mol1loop
+     end do atom2
+  end do atom1
+
   
   ! E(1,:) => Energy of relationa A
   ! E(2,:) => Energy of relationa B
@@ -132,51 +175,65 @@ PROGRAM SCC_Disp
   ! E(:,2) => Energy from D3
 
   write(*,*) '#  ',inputcoofile
-  write(*,'("A-TS  ",f10.3)') E(1,1)
-  write(*,'("A-D3  ",f10.3)') E(1,2)
-  write(*,'("B-TS  ",f10.3)') E(2,1)
-  write(*,'("B-D3  ",f10.3)') E(2,2)
+  write(*,'("A-TS  ",f10.3)') E(1,1) * HartKcalMol
+  write(*,'("A-D3  ",f10.3)') E(1,2) * HartKcalMol
+  write(*,'("B-TS  ",f10.3)') E(2,1) * HartKcalMol
+  write(*,'("B-D3  ",f10.3)') E(2,2) * HartKcalMol
+
+!!$  do i = 1,100     ! debug
+!!$     !     write(0,*) bab,bb,ba      ! debug
+!!$     kkk =  fdamp(1d0,dble(i)/10)     ! debug
+!!$     print*, kkk     ! debug
+!!$     !     write(*,*) fdamp(dble(1),dble(i/10))     ! debug
+!!$  end do
 
 
-!  do i = 1,natom/2
-!     r12 = dist(mol1(i)%coord,mol2(j)%coord)
-!  end do
+  
+!  do i = 1,natom/2       ! debug
+!     r12 = dist(mol1(i)%coord,mol2(j)%coord)      ! debug
+!  end do      ! debug
 
 
-!  call getdata('atomic_charges','results.tag')
-! write(*,*) (((matrix(i,j,k), i=1,ifrmt(1)),j=1,ifrmt(2)),k=1,ifrmt(3))
-!  write(*,*) coords
+!  call getdata('atomic_charges','results.tag')      ! debug
+! write(*,*) (((matrix(i,j,k), i=1,ifrmt(1)),j=1,ifrmt(2)),k=1,ifrmt(3))      ! debug
+!  write(*,*) coords      ! debug
 CONTAINS
 
-  real(kr) FUNCTION dist(r1,r2)
-!    USE precision
+  real(kr) FUNCTION bmix(bi,bj)
     IMPLICIT NONE
-    real(kr),intent(IN),dimension(3) :: r1,r2
-    real(kr),dimension(3,2) :: r12
-    integer(ki) :: i
-    
-    r12(:,1) = r1(:)
-    r12(:,2) = r2(:)
+    real(kr),intent(IN) :: bi,bj
 
-    do i = 1,3
-       dist = dist + ( r12(i,1) - r12(i,2) ) **2
-!       print*, dist  ! debug
-    end do
-
-    dist = sqrt(dist)
+    bmix = 2 * bi * bj / (bi + bj)
     
-  END FUNCTION dist
+  END FUNCTION bmix
+
+  real(kr) FUNCTION basym(b0,alpha,N,Z)
+    IMPLICIT NONE
+    real(kr),intent(IN) :: b0,alpha,N
+    integer(ki),intent(IN) :: Z
+    
+    basym = b0 * ( 1 / alpha )**(1./3.) * ( real(Z) / N )**(1./3.)
+    
+!    print*, 'basym', b0,alpha,Z,N  ! debug
+    
+  END FUNCTION basym
   
-    
-!!$  FUNCTION get_C6eff(name,Ni)
-!!$    USE atom_data
-!!$    IMPLICIT NONE
-!!$    real(kr),dimension(2) :: C6eff
-!!$    real(kr),intent(IN) :: Ni
-!!$    character(*),intent(IN) :: name
-!!$    
-!!$    
-!!$  END FUNCTION Get_C6eff
+  real(kr) FUNCTION fdamp(b,R)
+    IMPLICIT NONE
+    real(kr),intent(IN) :: b,R
+!    real(kr) :: asd
+    integer(ki) :: i
 
+    fdamp = 0.0
+!    print*, '-----------------> ',R ! debug
+    do i = 0,6
+       fdamp = fdamp + (b*R)**i / fact(i)
+!       print*, i,asd,fact(i),(b*R)**i / fact(i)  ! debug
+    end do
+    fdamp = 1 - exp( -b * R) * fdamp
+!    print*, 'fdamp ',fdamp ! debug
+    
+  END FUNCTION fdamp
+  
 END PROGRAM SCC_Disp
   
